03b36b78de5ba665d5bd2e699a1939858d598849
rodrigarc <rodrigo.arcoverdi@gmail.com>
2021-06-26 14:26:01 +0200

    code to download protein data

---
+++ src/download_data.py
@@ -0,0 +1,15 @@
+
+# USAGE
+# python download_data.py
+#
+# dependencies: 
+#   wget
+#
+# AIM:
+# Download the protein interactions to create a network analysis for the posterior analysis
+
+import wget
+
+url = "https://stringdb-static.org/download/protein.links.v11.0/9606.protein.links.v11.0.txt.gz"
+path = '../data/'
+wget.download(url,out = path)
